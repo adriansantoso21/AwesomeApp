1e7947eca89058d5fc7e82769478e7284f2cdfe8
adriansantoso21 <mkadriansantoso@gmail.com>
2021-05-04 12:28:34 +0700

    finish

---
+++ Awesome_App/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Awesome_App.Models;
+using Awesome_App.ViewModels;
+using Awesome_App.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Awesome_App.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ Awesome_App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Awesome_App.Models;
+using Awesome_App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Awesome_App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
